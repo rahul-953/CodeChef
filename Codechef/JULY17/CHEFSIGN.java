5ce75f74c68f3ac50a53dbc92da4508ddf62db2d
rahul <rahul@localhost>
2017-07-11 20:10:32 +0530

    updating CHEFSIGN

--- Codechef/JULY17/CHEFSIGN.java
+++ Codechef/JULY17/CHEFSIGN.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class CHEFSIGN {
+class CHEFSIGN {
 
 	public static void main(String[] args) throws IOException {
 
