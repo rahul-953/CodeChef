baf3b223131d9249f36653f9aa44c3777bc3eb80
rahul <rahul@localhost>
2017-07-11 23:52:40 +0530

    commit before merging

--- Codechef/JULY17/CHEFSIGN.java
+++ Codechef/JULY17/CHEFSIGN.java
@@ -2,6 +2,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/*
+ *  5>4>3>2>1<2<3<4<5
+ */
+
 class CHEFSIGN {
 
 	public static void main(String[] args) throws IOException {
@@ -11,24 +15,27 @@ class CHEFSIGN {
 
 		while (testCases-- > 0) {
 			String sign = br.readLine();
+			sign = sign.replaceAll("=", "");
 			int len = sign.length();
-			String updated_sign = "";
-			for (int i = 0; i < len; i++) {
-				if (sign.charAt(i) != '=') {
-					updated_sign += sign.charAt(i);
-				}
+
+			if (len == 0) {
+				System.out.println("1");
+				continue;
 			}
 
-			len = updated_sign.length();
 			int max = 1;
 			int global_max = 1;
 			for (int index = 1; index < len; index++) {
-				if (updated_sign.charAt(index) == updated_sign.charAt(index - 1)) {
+				if (sign.charAt(index) == (sign.charAt(index - 1))) {
 					max++;
+					global_max = (int) Math.max(max, global_max);
+
 				} else {
 					global_max = (int) Math.max(max, global_max);
 					max = 0;
 				}
+
+				// System.out.println("Max = " + max + " global_max = " + global_max);
 			}
 
 			System.out.println(global_max + 1);
