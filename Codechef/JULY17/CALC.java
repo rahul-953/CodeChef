40e266d9b26c33496bb8c5b4336d8464e07209a8
rahul <rahul@localhost>
2017-07-14 23:52:59 +0530

    CALC final

--- Codechef/JULY17/CALC.java
+++ Codechef/JULY17/CALC.java
@@ -7,7 +7,7 @@ import java.util.*;
 class CALC {
 
 	public static void main(String[] args) throws IOException {
-		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		PrintWriter pw = new PrintWriter(System.out);
 		Scanner sc = new Scanner(System.in);
 		int testCases = sc.nextInt();
@@ -16,21 +16,21 @@ class CALC {
 		String input[];
 
 		while (testCases-- > 0) {
-			//input = br.readLine().split(" ");
-			//N = Long.parseLong(input[0]);
-			//B = Long.parseLong(input[1]);
+			// input = br.readLine().split(" ");
+			// N = Long.parseLong(input[0]);
+			// B = Long.parseLong(input[1]);
 			N = sc.nextLong();
 			B = sc.nextLong();
 			long Ynum2 = 0L, Ynum1 = 0L;
-			long xclicks,yclicks;
-			
-			long num1 = (N/B)/2;
-			long num2 = num1+1;
-			
-			Ynum1 = (N - num1*B)*num1;
-			Ynum2 = (N - num2*B)*num2;
-			long Ynum = (long)Math.max(Ynum1,Ynum2);
-			
+			long xclicks, yclicks;
+
+			long num1 = (N / B) / 2;
+			long num2 = num1 + 1;
+
+			Ynum1 = (N - num1 * B) * num1;
+			Ynum2 = (N - num2 * B) * num2;
+			long Ynum = (long) Math.max(Ynum1, Ynum2);
+
 			pw.println(Ynum);
 		}
 
